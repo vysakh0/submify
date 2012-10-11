5eb1fdd701e4c3eeb454f6e992f03fbfc52d1fb0
vysakh0 <diplomatv@gmail.com>
2012-10-11 21:01:27 +0530

    change the model file of user (sign up)

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,17 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+
+  has_secure_password
 
   before_save { |user| user.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false}
+  validates :email, presence: true, 
+                    format: { with: VALID_EMAIL_REGEX }, 
+                    uniqueness: { case_sensitive: false} 
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
 end
