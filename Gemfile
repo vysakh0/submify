463cab54cc083b648549f91c18fbf205c7b0ca12
vysakh0 <diplomatv@gmail.com>
2012-10-14 14:12:15 +0530

    finish user signup

--- Gemfile
+++ Gemfile
@@ -17,7 +17,6 @@ end
 group :assets do
   gem 'sass-rails',   '3.2.5'
   gem 'coffee-rails', '3.2.2'
-
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platforms => :ruby
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+     redirect_to @user 
+    else
+      render 'new'
+    end
+  end
   def show
     @user = User.find(params[:id])
   end
