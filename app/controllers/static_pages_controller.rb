bbedb0a99de3226aa26d74dd0dba95b0032fa01b
vysakh0 <diplomatv@gmail.com>
2012-10-24 16:27:33 +0530

    remove index, _feed.html

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,11 @@ class StaticPagesController < ApplicationController
     if signed_in?
     @link = current_user.links.build if signed_in?
     @feed_items = current_user.feed.paginate(page: params[:page])
+
+    respond_to do |format|
+      format.js
+      format.html
+    end
     end
   end
 
