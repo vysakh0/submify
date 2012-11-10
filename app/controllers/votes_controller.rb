31a2daabc555f3402fa5a71be1f2c04608e3caf9
vysakh0 <diplomatv@gmail.com>
2012-11-10 16:00:11 +0530

    add vote per link,

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,24 @@
 class VotesController < ApplicationController
-  
+  before_filter :get_parent, only: :create
   def create
-    @vote = Vote.create(params[:vote])
-    @votable = @vote.votable
+    @vote = Vote.new(params[:vote])
+    @vote.votable = @votable
+    @vote.save
     respond_to do |format|
       format.js
     end
   end
   
   def destroy
-    vote = Vote.find_by_id(params[:id]).destroy
+    vote = Vote.find_by_id(params[:id])
     @votable = vote.votable
+    vote.destroy
     respond_to do |format|
       format.js
     end
   end
-
+  private
+  def get_parent
+    @votable = Link.find_by_id(params[:link_id])
+  end
 end
