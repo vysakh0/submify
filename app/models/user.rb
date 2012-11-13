54e720e64bbc24271849556e9c3f6b768989e2da
vysakh0 <diplomatv@gmail.com>
2012-11-13 10:46:18 +0530

    remove password presence:true for fb auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,8 +44,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false} 
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+#  validates :password, presence: true, length: { minimum: 6 }
+#  validates :password_confirmation, presence: true
 
   include Tire::Model::Search
   include Tire::Model::Callbacks
