de627557a3c9f90c9aa954f9ec9f6c3dab240bea
vysakh0 <diplomatv@gmail.com>
2012-11-15 12:56:51 +0530

    fix opengraph error associated with heroku prob: post action to fb using fb_graph when user submits a post

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -76,8 +76,6 @@ class LinksController < ApplicationController
       app = FbGraph::Application.new("295241533825642")
       me = FbGraph::User.me(current_user.oauth_token)
       action = me.og_action!(
-          app.og_action(:submit), # or simply "APP_NAMESPACE:ACTION" as String
-          :post => link_url(@link) 
       )
     end
     def check_url
