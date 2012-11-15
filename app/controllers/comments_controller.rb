8ca93de50c9bc7c8c8c554899fdea23452435406
vysakh0 <diplomatv@gmail.com>
2012-11-15 19:06:22 +0530

    add submit link, comment to opengraph

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,44 +4,54 @@ class CommentsController < ApplicationController
   def new
     @comment = @parent.comments.build
     @comment.user = current_user
+    publish_to_fb if @parent.class.to_s == "Link"
   end
 
- def show
+  def show
 
-	@comment = Comment.find_by_id(params[:id])
-	@comments = @comment.comments.paginate(page: params[:page])
- end
+    @comment = Comment.find_by_id(params[:id])
+    @comments = @comment.comments.paginate(page: params[:page])
+  end
 
   def create
     @comment = @parent.comments.build(params[:comment])
     @comment.user = current_user
     if @comment.save
       flash[:notice] = "Commented"
+      publish_to_fb if @parent.class.to_s == "Link"
       redirect_to @parent
     else
       render :new
     end
   end
-  
+
 
   def destroy
     @comment.destroy if @comment
     redirect_to @parent 
   end
 
-protected
-  
-def get_parent
-  @parent = Link.find_by_id(params[:link_id]) if params[:link_id]
-  @parent = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
-end
+  protected
 
-def correct_user
-       
-       @comment = current_user.comments.find_by_id(params[:id])
-	@parent = @comment.commentable       
-	redirect_to @parent if @comment.nil?
-       
-end
+  def get_parent
+    @parent = Link.find_by_id(params[:link_id]) if params[:link_id]
+    @parent = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+  end
 
+  def correct_user
+
+    @comment = current_user.comments.find_by_id(params[:id])
+    @parent = @comment.commentable       
+    redirect_to @parent if @comment.nil?
+
+  end
+
+  def publish_to_fb
+    app = FbGraph::Application.new("295241533825642")
+    me = FbGraph::User.me(current_user.oauth_token)
+    action = me.og_action!(
+      app.og_action(:comment), # or simply "APP_NAMESPACE:ACTION" as String
+      :website => comment_url(@comment)
+    )
+  end
 end
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -77,10 +77,7 @@ class LinksController < ApplicationController
       me = FbGraph::User.me(current_user.oauth_token)
       action = me.og_action!(
           app.og_action(:submit), # or simply "APP_NAMESPACE:ACTION" as String
-          :link => link_url(@link), 
-          :website => @link.url_link,
-          :url => link_url(@link),
-          :title => @link.url_heading
+          :website => link_url(@link)
       )
     end
     def check_url
