1d0758d5e0872f8f85b55982d7a33d27618f032e
vysakh0 <diplomatv@gmail.com>
2012-11-15 13:27:52 +0530

    add actions for links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -75,6 +75,13 @@ class LinksController < ApplicationController
     def publish_to_fb
       app = FbGraph::Application.new("295241533825642")
       me = FbGraph::User.me(current_user.oauth_token)
+      action = me.og_action!(
+          app.og_action(:submit), # or simply "APP_NAMESPACE:ACTION" as String
+          :link => link_url(@link), 
+          :website => @link.url_link,
+          :url => link_url(@link),
+          :title => @link.url_heading
+      )
     end
     def check_url
 	given =params[:link][:url_link]
