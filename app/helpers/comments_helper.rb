dfe7c27f1dbe0fe96117e1c552d34c6e23eaf5f6
vysakh0 <diplomatv@gmail.com>
2012-12-11 23:51:07 +0530

    change date format to 11 Dec 2012 from 11-12-12

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,11 +1,12 @@
 module CommentsHelper
 
   def timing comment_time
+    date = comment_time.to_date
 
-    if Time.now.to_date == comment_time.to_date
+    if Time.now.to_date == date 
       comment_time.strftime("%H:%M")
     else
-      comment_time.strftime("%d-%m-%y")
+      date
     end
   end
 
