2d351b0464898eb1137c27db5a9890634291d34b
vysakh0 <diplomatv@gmail.com>
2012-12-29 16:39:21 +0530

    make use of thumb and large in paperclip

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -16,7 +16,7 @@ class Comment < ActiveRecord::Base
   attr_accessible :body, :avatar
   self.per_page = 10
 
-  has_attached_file :avatar, styles: { medium: "600x600>", thumb: "300x300>" }
+  has_attached_file :avatar, styles: { medium: ["600x600>", :jpg] }
 
   belongs_to :commentable, polymorphic: true, touch: true 
   belongs_to :user, touch: true 
