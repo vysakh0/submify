65125f5b4af847bd6014b761d55cd0714049ec15
vysakh0 <diplomatv@gmail.com>
2013-01-14 15:30:38 +0530

    add sshforwarding bit

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,7 @@ set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:keys] = ["~/.ec2/gsg-keypair"]
+ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
