aec279ad707d6c6e91c833cea06239a86481f65a
vysakh0 <diplomatv@gmail.com>
2013-01-14 22:13:16 +0530

    making ssh forwardng true

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,6 @@ set :repository, "git@bitbucket.org:vysakh0/submify.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
-ssh_options[:keys] = ["~/.ec2/gsg-keypair"]
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
