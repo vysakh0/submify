2ca2e5744098056fcf1bf57959736666ee04bffc
vysakh0 <diplomatv@gmail.com>
2013-01-16 18:54:57 +0530

    add initializer for soulmate&sideqik

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,10 @@
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://:ec2-50-17-104-118.compute-1.amazonaws.com:6379'}
+end
+# Next, you need to configure the Sidekiq client, which is similar.
+# If you're using the client with a single-threaded Rails (or other ruby) process,
+# add a size of 1, which will provide one Redis connection for the client:
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://:ec2-50-17-104-118.compute-1.amazonaws.com:6379'}
+end
---
+++ config/initializers/soulmate.rb
@@ -0,0 +1,1 @@
+Soulmate.redis = Redis.new(:host => 'ec2-50-17-104-118.compute-1.amazonaws.com' , :port => 6379)
