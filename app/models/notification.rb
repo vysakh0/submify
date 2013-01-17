62b51e20914bc2bb38685be80d3680c2c66e0efd
vysakh0 <diplomatv@gmail.com>
2013-01-18 00:11:36 +0530

    remove counter cache notifications

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,9 +2,17 @@ class Notification < ActiveRecord::Base
   attr_accessible :notifiable_id, :notifiable_type, :parent_id, :parent_type
 
   attr_accessible :user_id
-  belongs_to :user, counter_cache: true
+  belongs_to :user
   belongs_to :notifiable, polymorphic: true
   belongs_to :parent, polymorphic: true
   validates :user_id, presence: true
+  after_save :inc_counter
+  after_destroy :dec_counter
 
+  def inc_counter
+    user.update_column(:notifications_count, user.notifications_count + 1)
+  end 
+  def dec_counter
+    user.update_column(:notifications_count, user.notifications_count - 1)
+  end 
 end
