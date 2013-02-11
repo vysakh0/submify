aa54e9f58f5f2cc258ab7d30c36ae2849ec7c8b6
vysakh0 <diplomatv@gmail.com>
2013-02-12 01:58:28 +0530

    remove swp files, add system,imgs to gitignore

--- config/initializers/sidekiq_example.rb
+++ config/initializers/sidekiq_example.rb
@@ -5,6 +5,6 @@
 # If you're using the client with a single-threaded Rails (or other ruby) process,
 # add a size of 1, which will provide one Redis connection for the client:
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://xyz.servername.com:6379'}
-end
+#Sidekiq.configure_client do |config|
+  #config.redis = { url: 'redis://xyz.servername.com:6379'}
+#end
