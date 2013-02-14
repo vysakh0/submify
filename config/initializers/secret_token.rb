d9c90867692326382191dc1f333b6c206ee3a236
vysakh0 <diplomatv@gmail.com>
2013-02-15 01:45:44 +0530

    remove cached files, & untrack public dir

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Youarel::Application.config.secret_token = 'f690f2530dc761afc237bce7590a1843b6d3373b4296ce3deba50abe34d8a28a85ea0716962d046def7ec9f374cf7b2df31853e1fcf99cda3fcc244a5e564571'
