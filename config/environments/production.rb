f6a554fb17224c7d0383999770fecc0625013a01
vysakh0 <diplomatv@gmail.com>
2013-02-27 12:31:17 +0530

    change name of app from Youarel to Submify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Youarel::Application.configure do
+Submify::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Youarel::Application.config.secret_token = 'f690f2530dc761afc237bce7590a1843b6d3373b4296ce3deba50abe34d8a28a85ea0716962d046def7ec9f374cf7b2df31853e1fcf99cda3fcc244a5e564571'
+Submify::Application.config.secret_token = 'f690f2530dc761afc237bce7590a1843b6d3373b4296ce3deba50abe34d8a28a85ea0716962d046def7ec9f374cf7b2df31853e1fcf99cda3fcc244a5e564571'
