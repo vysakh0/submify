96fe7cb732c53d7a6fce42a9000c3793be782f81
Saurav Chakraborty <csaurav80@gmail.com>
2013-03-03 17:43:36 +0530

    verify a new user in a development mode - not by using mail

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe User do
   
-  it "can verify a User without sending a mail" do
+  it "can verify a new user without sending a mail" do
   	user = User.create!({:name => 'Saurav C', :email => 'csaurav80@gmail.com', :password => 'Submify@12345', 
   		:password_confirmation => 'Submify@12345'})
   	expect(user.verify).to eq false
