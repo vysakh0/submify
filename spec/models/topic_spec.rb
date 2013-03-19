673100980f444e60be09d7bee7086b04132bcd83
Saurav Chakraborty <csaurav80@gmail.com>
2013-03-19 14:21:50 +0530

    removed before(:all) from spec

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -18,11 +18,10 @@
 require 'spec_helper'
 
 describe Topic do
-  before(:all) do
-    @topic = FactoryGirl.build(:topic) 
-  end
   
-  it "should be able to return similar matches" do
+  before { @topic = FactoryGirl.build(:topic) }
+
+  it "is able to return similar matches" do
   	matched_results = Topic.search(@topic.name)
   	matched_results.each do |result|
   		result['label'].should == @topic.name
